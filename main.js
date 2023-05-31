ce38506016477efc37c649e2917cbe2a2fcbd612
Erickhrs <erickhenriquee4444@gmail.com>
2023-05-31 16:58:12 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,175 @@
+
+const colors = [
+    "AliceBlue",
+    "AntiqueWhite",
+    "Aqua",
+    "Aquamarine",
+    "Azure",
+    "Beige",
+    "Bisque",
+    "Black",
+    "BlanchedAlmond",
+    "Blue",
+    "BlueViolet",
+    "Brown",
+    "BurlyWood",
+    "CadetBlue",
+    "Chartreuse",
+    "Chocolate",
+    "Coral",
+    "CornflowerBlue",
+    "Cornsilk",
+    "Crimson",
+    "Cyan",
+    "DarkBlue",
+    "DarkCyan",
+    "DarkGoldenRod",
+    "DarkGray",
+    "DarkGrey",
+    "DarkGreen",
+    "DarkKhaki",
+    "DarkMagenta",
+    "DarkOliveGreen",
+    "DarkOrange",
+    "DarkOrchid",
+    "DarkRed",
+    "DarkSalmon",
+    "DarkSeaGreen",
+    "DarkSlateBlue",
+    "DarkSlateGray",
+    "DarkSlateGrey",
+    "DarkTurquoise",
+    "DarkViolet",
+    "DeepPink",
+    "DeepSkyBlue",
+    "DimGray",
+    "DimGrey",
+    "DodgerBlue",
+    "FireBrick",
+    "FloralWhite",
+    "ForestGreen",
+    "Fuchsia",
+    "Gainsboro",
+    "GhostWhite",
+    "Gold",
+    "GoldenRod",
+    "Gray",
+    "Grey",
+    "Green",
+    "GreenYellow",
+    "HoneyDew",
+    "HotPink",
+    "IndianRed",
+    "Indigo",
+    "Ivory",
+    "Khaki",
+    "Lavender",
+    "LavenderBlush",
+    "LawnGreen",
+    "LemonChiffon",
+    "LightBlue",
+    "LightCoral",
+    "LightCyan",
+    "LightGoldenRodYellow",
+    "LightGray",
+    "LightGrey",
+    "LightGreen",
+    "LightPink",
+    "LightSalmon",
+    "LightSeaGreen",
+    "LightSkyBlue",
+    "LightSlateGray",
+    "LightSlateGrey",
+    "LightSteelBlue",
+    "LightYellow",
+    "Lime",
+    "LimeGreen",
+    "Linen",
+    "Magenta",
+    "Maroon",
+    "MediumAquaMarine",
+    "MediumBlue",
+    "MediumOrchid",
+    "MediumPurple",
+    "MediumSeaGreen",
+    "MediumSlateBlue",
+    "MediumSpringGreen",
+    "MediumTurquoise",
+    "MediumVioletRed",
+    "MidnightBlue",
+    "MintCream",
+    "MistyRose",
+    "Moccasin",
+    "NavajoWhite",
+    "Navy",
+    "OldLace",
+    "Olive",
+    "OliveDrab",
+    "Orange",
+    "OrangeRed",
+    "Orchid",
+    "PaleGoldenRod",
+    "PaleGreen",
+    "PaleTurquoise",
+    "PaleVioletRed",
+    "PapayaWhip",
+    "PeachPuff",
+    "Peru",
+    "Pink",
+    "Plum",
+    "PowderBlue",
+    "Purple",
+    "Red",
+    "RosyBrown",
+    "RoyalBlue",
+    "SaddleBrown",
+    "Salmon",
+    "SandyBrown",
+    "SeaGreen",
+    "SeaShell",
+    "Sienna",
+    "Silver", "SkyBlue"]
+var vzs = 0;
+var i = 0;
+var x = 0;
+let hist_array = [];
+
+const infosc = document.querySelector("#colord");
+function mudafundo() {
+    //mudar cor
+    let icolor = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = (colors[icolor]);
+    document.querySelector("#colord").innerHTML = (colors[icolor] + " | N°: " + icolor)
+
+    //infos na tela e histórico
+    vzs = parseInt(vzs + 1);
+    hist_array.push(colors[icolor] + " | N°: " + icolor);
+    let novalinha = document.createElement('tr')
+    let novainfo = document.createElement('td')
+    const histab = document.querySelector('table');
+    if (vzs <= 20) {
+        novainfo.textContent = (hist_array[i]);
+        novalinha.appendChild(novainfo)
+        histab.appendChild(novalinha)
+        i++
+    }
+    else {
+        var ultimos20 = hist_array.slice(-20);
+        const linhashis = document.querySelectorAll('tr');
+        linhashis.forEach(tr => tr.remove())
+        while (x < ultimos20.length) {
+            novalinha = document.createElement('tr')
+            novainfo = document.createElement('td')
+            novainfo.textContent = (ultimos20[x]);
+            novalinha.appendChild(novainfo)
+            histab.appendChild(novalinha)
+            i++
+            x++
+        }
+
+    }
+    x = 0;
+
+
+
+}
